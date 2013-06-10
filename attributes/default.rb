b804f09953d874ab1a5750cefa282744d7c09c9a
Lars Kleen <l.kleen@bigpoint.net>
2013-06-10 11:51:57 +0200

    added attributes for rvm cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,4 +12,80 @@ default['kibana']['elasticsearch']['host'] = '127.0.0.1'
 default['kibana']['elasticsearch']['port'] = 9200
 default['kibana']['rubyversion'] = '1.9.3'
 
+set['rvm']['ruby']['version'] = node['kibana']['rubyversion']
+set['rvm']['global_gems'] = [
+        {
+            'name'    => 'bundler',
+            'version' => '1.1.5'
+        },
+        {
+            'name'    => 'rake',
+            'version' => '10.0.4'
+        },
+        {
+            'name'    => 'rubygems-bundler',
+            'action'  => 'remove'
+        },
+        {
+            'name'    => 'daemons',
+            'version' => '1.1.9'
+        },
+        {
+            'name'    => 'diff-lcs',
+            'version' => '1.1.3'
+        },
+        {
+            'name'    => 'eventmachine',
+            'version' => '1.0.0'
+        },
+        {
+            'name'    => 'fastercsv',
+            'version' => '1.5.5'
+        },
+        {
+            'name'    => 'json',
+            'version' => '1.7.5'
+        },
+        {
+            'name'    => 'rack',
+            'version' => '1.4.1'
+        },
+        {
+            'name'    => 'rack-protection',
+            'version' => '1.2.0'
+        },
+        {
+            'name'    => 'tilt',
+            'version' => '1.3.3'
+        },
+        {
+            'name'    => 'sinatra',
+            'version' => '1.3.3'
+        },
+        {
+            'name'    => 'thin',
+            'version' => '1.5.0'
+        },
+        {
+            'name'    => 'tzinfo',
+            'version' => '0.3.35'
+        },
+        {
+            'name'    => 'rspec-core',
+            'version' => '2.11.1'
+        },
+        {
+            'name'    => 'rspec-expectations',
+            'version' => '2.11.3'
+        },
+        {
+            'name'    => 'rspec-mocks',
+            'version' => '2.11.3'
+        },
+        {
+            'name'    => 'rspec',
+            'version' => '2.11.0'
+        }
+    ]
+
 
--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.5'
 
 depends 'build-essential'
+depends 'rvm'
 suggests 'authbind'
 suggests 'apt'
